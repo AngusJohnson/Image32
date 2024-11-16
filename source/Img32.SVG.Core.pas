AngusJohnson/Image32

commit 7465b8933be99701ff00730df9c06521eb199770

Img32.Text: Added support for 4-byte chars (USC-4) (#111)

source/Img32.SVG.Core.pas
@@ -1,4 +1,4 @@
-unit Img32.SVG.Core;
+﻿unit Img32.SVG.Core;
 
 (*******************************************************************************
 * Author    :  Angus Johnson                                                   *
@@ -95,8 +95,11 @@ type
   TFontDecoration = (fdUndefined, fdNone, fdUnderline, fdStrikeThrough);
   TSvgTextAlign = (staUndefined, staLeft, staCenter, staRight, staJustify);
 
+  UTF8Strings = array of UTF8String;
+
   TSVGFontInfo = record
-    family      : TTtfFontFamily;
+    family      : TFontFamily;
+    nameOptions : UTF8Strings;
     size        : double;
     spacing     : double;
     textLength  : double;
@@ -259,6 +262,9 @@ type
   function SkipBlanksEx(c: PUTF8Char; endC: PUTF8Char): PUTF8Char; {$IFDEF INLINE} inline; {$ENDIF}
   function SkipBlanksAndComma(c, endC: PUTF8Char): PUTF8Char; {$IFDEF INLINE} inline; {$ENDIF}
 
+  function GetCommaSeparatedArray(const str: UTF8String): UTF8Strings;
+  function TrimQuotes(const str: UTF8String): UTF8String;
+
   procedure ConvertUnicodeToUtf8(memStream: TMemoryStream);
 
   function GetScale(src, dst: double): double;
@@ -268,7 +274,6 @@ type
 
 type
   TSetOfUTF8Char = set of UTF8Char;
-  UTF8Strings = array of UTF8String;
 
 function CharInSet(chr: UTF8Char; const chrs: TSetOfUTF8Char): Boolean;
 
@@ -604,6 +609,59 @@ begin
     while (j <= len) and (str[j] <= #32) do inc(j);
     i := j;
   end;
+end;
+//------------------------------------------------------------------------------
+
+function TrimQuotes(const str: UTF8String): UTF8String;
+var
+  i, len: integer;
+begin
+  len := Length(str);
+  i := 1;
+  while (i < len) and (str[i] <= space) do inc(i);
+
+  if (i < len) and (str[i] in [quote, dquote]) then
+  begin
+    inc(i);
+    while (len > i) and not (str[len] in [quote, dquote]) do dec(len);
+    if len = i then
+      Result := str else
+      Result := Copy(str, i, len - i);
+  end
+  else
+    Result := str
+end;
+//------------------------------------------------------------------------------
+
+function GetCommaSeparatedArray(const str: UTF8String): UTF8Strings;
+var
+  i,j,k, cnt, len: integer;
+const
+  separator: Utf8Char = ',';
+begin
+  // nb: separator is NOT escaped.
+  if separator = #32 then
+  begin
+    Result := Split(str);
+    Exit;
+  end;
+
+  len := Length(str);
+  cnt := 1;
+  for i := 1 to len do
+    if (str[i] = separator) then inc(cnt);
+  SetLength(Result, cnt);
+  j := 0;
+  k := 1;
+  for i := 1 to len do
+  begin
+    if (str[i] <> separator) then Continue;
+    Result[j] := TrimQuotes(Copy(str, k, i-k));
+    inc(j);
+    k := i + 1;
+  end;
+  if len >= k then
+    Result[j] := TrimQuotes(Copy(str, k, len-k +1));
 end;
 //------------------------------------------------------------------------------
 
@@ -1438,12 +1496,12 @@ begin
     begin
       hash := ParseNextWordHashed(c, endC);
       case hash of
-        hSans_045_Serif   : fontInfo.family := ttfSansSerif;
-        hSerif            : fontInfo.family := ttfSerif;
-        hMonospace        : fontInfo.family := ttfMonospace;
+        hSans_045_Serif   : fontInfo.family := tfSansSerif;
+        hSerif            : fontInfo.family := tfSerif;
+        hMonospace        : fontInfo.family := tfMonospace;
         hBold             : fontInfo.weight := 600;
         hItalic           : fontInfo.italic := sfsItalic;
-        hNormal           : 
+        hNormal           :
           begin
             fontInfo.weight := 400;
             fontInfo.italic := sfsNone;

source/Img32.SVG.Reader.pas
@@ -666,7 +666,7 @@ const
     strokeWidth: (rawVal: InvalidD; unitType: utNumber);
     strokeCap: esPolygon; strokeJoin: jsMiter; strokeMitLim: 0.0; strokeEl : '';
     dashArray: nil; dashOffset: 0;
-    fontInfo: (family: ttfUnknown; size: 0; spacing: 0.0;
+    fontInfo: (family: tfUnknown; familyNames: nil; size: 0; spacing: 0.0;
     textLength: 0; italic: sfsUndefined; weight: -1; align: staUndefined;
     decoration: fdUndefined; baseShift: (rawVal: InvalidD; unitType: utNumber));
     markerStart: ''; markerMiddle: ''; markerEnd: '';
@@ -782,8 +782,11 @@ begin
     if (filterElRef <> '') then
       drawDat.filterElRef := filterElRef;
 
-    if fontInfo.family <> ttfUnknown then
+    if fontInfo.family <> tfUnknown then
       drawDat.fontInfo.family := fontInfo.family;
+    if Assigned(fontInfo.familyNames) then
+      drawDat.fontInfo.familyNames := fontInfo.familyNames;
+
     if fontInfo.size > 0 then
       drawDat.fontInfo.size := fontInfo.size;
     if fontInfo.spacing <> 0 then
@@ -4288,15 +4291,18 @@ var
 begin
   with aOwnerEl.fDrawData.FontInfo do
   begin
-    family := ttfUnknown;
+    family := tfUnknown;
+    familyNames := GetCommaSeparatedArray(value);
+    // get comma separated family names
+
     c := PUTF8Char(value);
     endC := c + Length(value);
     while ParseNextWordExHash(c, endC, hash) do
     begin
       case hash of
-        hSans_045_Serif, hArial  : family := ttfSansSerif;
-        hSerif, hTimes: family := ttfSerif;
-        hMonospace: family := ttfMonospace;
+        hSans_045_Serif, hArial  : family := tfSansSerif;
+        hSerif, hTimes: family := tfSerif;
+        hMonospace: family := tfMonospace;
         else Continue;
       end;
       break;
@@ -5373,14 +5379,20 @@ end;
 
 procedure TSvgReader.GetBestFontForFontCache(const svgFontInfo: TSVGFontInfo);
 var
+  i, len: integer;
   bestFontReader: TFontReader;
   fi: TFontInfo;
 begin
-  if svgFontInfo.family = ttfUnknown then
-    fi.fontFamily := ttfSansSerif else
-    fi.fontFamily := svgFontInfo.family;
+  if svgFontInfo.family = tfUnknown then
+    fi.family := tfSansSerif else
+    fi.family := svgFontInfo.family;
   fi.faceName := ''; //just match to a family here, not to a specific facename
   fi.macStyles := [];
+  len := Length(svgFontInfo.familyNames);
+  SetLength(fi.familyNames, len);
+  for i := 0 to len -1 do
+    fi.familyNames[i] := string(svgFontInfo.familyNames[i]);
+
   if svgFontInfo.italic = sfsItalic then
     Include(fi.macStyles, msItalic);
   if svgFontInfo.weight >= 600 then

commit 85496b0036ecf76081ee685d1264ce5119d668cd

Img32.Text: fixed a bug introduced in a recent revision (7465b89).

source/Img32.SVG.Core.pas
@@ -3,7 +3,7 @@ unit Img32.SVG.Core;
 (*******************************************************************************
 * Author    :  Angus Johnson                                                   *
 * Version   :  4.6                                                             *
-* Date      :  14 November 2024                                                *
+* Date      :  16 November 2024                                                *
 * Website   :  http://www.angusj.com                                           *
 * Copyright :  Angus Johnson 2019-2024                                         *
 *                                                                              *
@@ -284,6 +284,7 @@ const
   quote       = '''';
   dquote      = '"';
   space       = #32;
+  comma       = ',';
   SvgDecimalSeparator = '.'; //do not localize
 
   {$I Img32.SVG.HashConsts.inc}
@@ -595,18 +596,18 @@ begin
   spcCnt := 0;
   i := 1;
   len := Length(str);
-  while (len > 0) and (str[len] <= #32) do dec(len);
-  while (i <= len) and (str[i] <= #32) do inc(i);
+  while (len > 0) and (str[len] <= space) do dec(len);
+  while (i <= len) and (str[i] <= space) do inc(i);
   for j := i + 1 to len do
-    if (str[j] <= #32) and (str[j -1] > #32) then inc(spcCnt);
+    if (str[j] <= space) and (str[j -1] > space) then inc(spcCnt);
   SetLength(Result, spcCnt +1);
   for k := 0 to spcCnt do
   begin
     j := i;
-    while (j <= len) and (str[j] > #32) do inc(j);
+    while (j <= len) and (str[j] > space) do inc(j);
     SetLength(Result[k], j -i);
     Move(str[i], Result[k][1], j -i);
-    while (j <= len) and (str[j] <= #32) do inc(j);
+    while (j <= len) and (str[j] <= space) do inc(j);
     i := j;
   end;
 end;
@@ -637,20 +638,18 @@ end;
 function GetCommaSeparatedArray(const str: UTF8String): UTF8Strings;
 var
   i,j,k, cnt, len: integer;
-const
-  separator: Utf8Char = ',';
 begin
   // precondition: commas CANNOT be embedded
   len := Length(str);
   cnt := 1;
   for i := 1 to len do
-    if (str[i] = separator) then inc(cnt);
+    if (str[i] = comma) then inc(cnt);
   SetLength(Result, cnt);
   j := 0;
   k := 1;
   for i := 1 to len do
   begin
-    if (str[i] <> separator) then Continue;
+    if (str[i] <> comma) then Continue;
     Result[j] := TrimQuotes(Copy(str, k, i-k));
     inc(j);
     k := i + 1;
@@ -1294,9 +1293,9 @@ var
   len: integer;
 begin
   // trim left
-  while (c < endC) and (c^ <= #32) do Inc(c);
+  while (c < endC) and (c^ <= space) do Inc(c);
   // trim right
-  while (endC > c) and (endC[-1] <= #32) do Dec(endC);
+  while (endC > c) and (endC[-1] <= space) do Dec(endC);
 
   len := endC - c;
   SetLength(S, len);

source/Img32.SVG.Reader.pas
@@ -3,7 +3,7 @@ unit Img32.SVG.Reader;
 (*******************************************************************************
 * Author    :  Angus Johnson                                                   *
 * Version   :  4.6                                                             *
-* Date      :  17 October 2024                                                 *
+* Date      :  16 November 2024                                                *
 * Website   :  http://www.angusj.com                                           *
 * Copyright :  Angus Johnson 2019-2024                                         *
 *                                                                              *

source/Img32.Text.pas
@@ -3,7 +3,7 @@ unit Img32.Text;
 (*******************************************************************************
 * Author    :  Angus Johnson                                                   *
 * Version   :  4.6                                                             *
-* Date      :  18 September 2024                                               *
+* Date      :  16 November 2024                                                *
 * Website   :  http://www.angusj.com                                           *
 * Copyright :  Angus Johnson 2019-2024                                         *
 * Purpose   :  TrueType fonts for TImage32 (without Windows dependencies)      *
@@ -88,7 +88,7 @@ type
   TFormat4Rec = record
     startCode    : Word;
     endCode      : Word;
-    idDelta       : Word;
+    idDelta      : Word;
     rangeOffset  : Word;
   end;
 
@@ -348,13 +348,12 @@ type
     fTbl_post          : TFontTable_Post;
     fTbl_loca2         : TArrayOfWord;
     fTbl_loca4         : TArrayOfCardinal;
-    fCmapTblRecs       : TArrayOfCmapTblRec;
     fKernTable         : TArrayOfKernRecs;
 
-    fCmapFormat        : Word;
-    fFormat0CodeMap    : array[0..255] of byte;
+    fFormat0CodeMap    : array of byte;
     fFormat4CodeMap    : array of TFormat4Rec;
     fFormat12CodeMap   : array of TFormat12Group;
+    fFormat4Offset     : integer;
 
     function GetTables: Boolean;
     function GetTable_name: Boolean;
@@ -945,7 +944,6 @@ end;
 procedure TFontReader.Clear;
 begin
   fTables               := nil;
-  fCmapTblRecs          := nil;
   fFormat4CodeMap       := nil;
   fFormat12CodeMap      := nil;
   fKernTable            := nil;
@@ -1199,11 +1197,14 @@ function TFontReader.GetTable_cmap: Boolean;
 var
   i,j         : integer;
   segCount    : integer;
+  format      : WORD;
   reserved    : WORD;
-  offset      : Cardinal;
   format4Rec  : TCmapFormat4;
   format12Rec : TCmapFormat12;
   cmapTbl     : TFontTable;
+  cmapTblRecs : array of TCmapTblRec;
+label
+  format4Error;
 begin
   Result := false;
   cmapTbl := fTables[fTblIdxes[tblCmap]];
@@ -1214,85 +1215,90 @@ begin
   GetWord(fStream, fTbl_cmap.numTables);
 
   //only use the unicode table (0: always first)
-  SetLength(fCmapTblRecs, fTbl_cmap.numTables);
+  SetLength(cmapTblRecs, fTbl_cmap.numTables);
   for i := 0 to fTbl_cmap.numTables -1 do
   begin
-    GetWord(fStream, fCmapTblRecs[i].platformID);
-    GetWord(fStream, fCmapTblRecs[i].encodingID);
-    GetCardinal(fStream, fCmapTblRecs[i].offset);
+    GetWord(fStream, cmapTblRecs[i].platformID);
+    GetWord(fStream, cmapTblRecs[i].encodingID);
+    GetCardinal(fStream, cmapTblRecs[i].offset);
   end;
 
-  // prefer USC-4 (encodingID=10) over USC-2 (encodingID=1)
-  j := -1;
-  for i := fTbl_cmap.numTables -1 downto 0 do
+  for i := 0 to fTbl_cmap.numTables -1 do
   begin
-    if (fCmapTblRecs[i].platformID <> 0) and
-      (fCmapTblRecs[i].platformID <> 3) then Continue;
-    j := i;
-    if fCmapTblRecs[i].encodingID = 10 then Break;
-  end;
-  if j < 0 then Exit;
-  fStream.Position := cmapTbl.offset + fCmapTblRecs[j].offset;
-  GetWord(fStream, fCmapFormat);
+    with cmapTblRecs[i] do
+      if (platformID = 0) or (platformID = 3) then
+        fStream.Position := cmapTbl.offset + offset
+      else
+        Continue;
+    GetWord(fStream, format);
 
-  case fCmapFormat of
-    0:
-      begin
-        GetWord(fStream, format4Rec.length);
-        GetWord(fStream, format4Rec.language);
-        for i := 0 to 255 do
-          GetByte(fStream, fFormat0CodeMap[i]);
-        fFontInfo.glyphCount := 255;
-      end;
-    4: //USC-2
-      begin
-        GetWord(fStream, format4Rec.length);
-        GetWord(fStream, format4Rec.language);
-
-        fFontInfo.glyphCount := 0;
-        GetWord(fStream, format4Rec.segCountX2);
-        segCount := format4Rec.segCountX2 shr 1;
-        GetWord(fStream, format4Rec.searchRange);
-        GetWord(fStream, format4Rec.entrySelector);
-        GetWord(fStream, format4Rec.rangeShift);
-        SetLength(fFormat4CodeMap, segCount);
-        for i := 0 to segCount -1 do
-          GetWord(fStream, fFormat4CodeMap[i].endCode);
-        if fFormat4CodeMap[segCount-1].endCode <> $FFFF then Exit; //error
-        GetWord(fStream, reserved);
-        if reserved <> 0 then Exit; //error
-        for i := 0 to segCount -1 do
-          GetWord(fStream, fFormat4CodeMap[i].startCode);
-        if fFormat4CodeMap[segCount-1].startCode <> $FFFF then Exit; //error
-        for i := 0 to segCount -1 do
-          GetWord(fStream, fFormat4CodeMap[i].idDelta);
-        offset := fStream.Position;
-        for i := 0 to segCount -1 do
+    case format of
+      0:
         begin
-          GetWord(fStream, fFormat4CodeMap[i].rangeOffset);
-          if fFormat4CodeMap[i].rangeOffset > 0 then
-            inc(fFormat4CodeMap[i].rangeOffset, offset);
+          if Assigned(fFormat0CodeMap) then Continue;
+          GetWord(fStream, format4Rec.length);
+          GetWord(fStream, format4Rec.language);
+          SetLength(fFormat0CodeMap, 256);
+          for j := 0 to 255 do
+            GetByte(fStream, fFormat0CodeMap[j]);
+          fFontInfo.glyphCount := 255;
         end;
-      end;
-    12: //USC-4
-      begin
-        GetWord(fStream, reserved);
-        GetCardinal(fStream, format12Rec.length);
-        GetCardinal(fStream, format12Rec.language);
-        GetCardinal(fStream, format12Rec.nGroups);
-        SetLength(fFormat12CodeMap, format12Rec.nGroups);
-        for i := 0 to format12Rec.nGroups -1 do
-          with fFormat12CodeMap[i] do
-          begin
-            GetCardinal(fStream, startCharCode);
-            GetCardinal(fStream, endCharCode);
-            GetCardinal(fStream, startGlyphCode);
-          end;
-      end;
-    else
-      Exit; // unsupported
+      4: //USC-2
+        begin
+          if Assigned(fFormat4CodeMap) then Continue;
+          GetWord(fStream, format4Rec.length);
+          GetWord(fStream, format4Rec.language);
+
+          fFontInfo.glyphCount := 0;
+          GetWord(fStream, format4Rec.segCountX2);
+          segCount := format4Rec.segCountX2 shr 1;
+          GetWord(fStream, format4Rec.searchRange);
+          GetWord(fStream, format4Rec.entrySelector);
+          GetWord(fStream, format4Rec.rangeShift);
+          SetLength(fFormat4CodeMap, segCount);
+          for j := 0 to segCount -1 do
+            GetWord(fStream, fFormat4CodeMap[j].endCode);
+          if fFormat4CodeMap[segCount-1].endCode <> $FFFF then
+            GoTo format4Error;
+          GetWord(fStream, reserved);
+          if reserved <> 0 then
+            GoTo format4Error;
+          for j := 0 to segCount -1 do
+            GetWord(fStream, fFormat4CodeMap[j].startCode);
+          if fFormat4CodeMap[segCount-1].startCode <> $FFFF then
+            GoTo format4Error;
+          for j := 0 to segCount -1 do
+            GetWord(fStream, fFormat4CodeMap[j].idDelta);
+
+          fFormat4Offset := fStream.Position;
+          for j := 0 to segCount -1 do
+            GetWord(fStream, fFormat4CodeMap[j].rangeOffset);
+          if Assigned(fFormat12CodeMap) then Break
+          else Continue;
+
+          format4Error:
+          fFormat4CodeMap := nil;
+        end;
+      12: //USC-4
+        begin
+          if Assigned(fFormat12CodeMap) then Continue;
+          GetWord(fStream, reserved);
+          GetCardinal(fStream, format12Rec.length);
+          GetCardinal(fStream, format12Rec.language);
+          GetCardinal(fStream, format12Rec.nGroups);
+          SetLength(fFormat12CodeMap, format12Rec.nGroups);
+          for j := 0 to format12Rec.nGroups -1 do
+            with fFormat12CodeMap[j] do
+            begin
+              GetCardinal(fStream, startCharCode);
+              GetCardinal(fStream, endCharCode);
+              GetCardinal(fStream, startGlyphCode);
+            end;
+          if Assigned(fFormat4CodeMap) then Break;
+        end;
+    end;
   end;
-  Result := true;
+  Result := Assigned(fFormat4CodeMap) or Assigned(fFormat12CodeMap);
 end;
 //------------------------------------------------------------------------------
 
@@ -1302,32 +1308,36 @@ var
   w: WORD;
 begin
   result := 0; //default to the 'missing' glyph
-  case fCmapFormat of
-    0: if codePoint <= 255 then Result := fFormat0CodeMap[codePoint];
-    4:
-      for i := 0 to High(fFormat4CodeMap) do
-        with fFormat4CodeMap[i] do
-          if codePoint <= endCode then
+  if (codePoint < 256) and Assigned(fFormat0CodeMap) then
+    Result := fFormat0CodeMap[codePoint]
+  else if Assigned(fFormat12CodeMap) then
+  begin
+    for i := 0 to High(fFormat12CodeMap) do
+      with fFormat12CodeMap[i] do
+        if codePoint <= endCharCode then
+        begin
+          if codePoint < startCharCode then Break;
+          result := (startGlyphCode + Word(codePoint) - startCharCode);
+          Break;
+        end;
+  end
+  else if (codePoint < $FFFF) and Assigned(fFormat4CodeMap) then
+  begin
+    for i := 0 to High(fFormat4CodeMap) do
+      with fFormat4CodeMap[i] do
+        if codePoint <= endCode then
+        begin
+          if codePoint < startCode then Break;
+          if rangeOffset > 0 then
           begin
-            if codePoint < startCode then Break;
-            if rangeOffset > 0 then
-            begin
-              fStream.Position := rangeOffset + 2 * (i + Word(codePoint) - startCode);
-              GetWord(fStream, w);
-              if w < fTbl_maxp.numGlyphs then Result := w;
-            end else
-              result := (idDelta + codePoint) and $FFFF;
-            Break;
-          end;
-    12:
-      for i := 0 to High(fFormat12CodeMap) do
-        with fFormat12CodeMap[i] do
-          if codePoint <= endCharCode then
-          begin
-            if codePoint < startCharCode then Break;
-            result := (startGlyphCode + Word(codePoint) - startCharCode);
-            Break;
-          end;
+            fStream.Position := fFormat4Offset +
+              rangeOffset + 2 * (i + Word(codePoint) - startCode);
+            GetWord(fStream, w);
+            if w < fTbl_maxp.numGlyphs then Result := w;
+          end else
+            result := (idDelta + codePoint) and $FFFF;
+          Break;
+        end;
   end;
 end;
 //------------------------------------------------------------------------------
@@ -2029,7 +2039,7 @@ begin
   glyphIdx := GetGlyphIdxUsingCmap(unicode);
   if (glyphIdx = 0) then
   begin
-    if (unicode > 32) and Assigned(fFontManager)  then
+    if (unicode > 32) and Assigned(fFontManager) then
       glyphIdx := fFontManager.FindReaderContainingGlyph(unicode,
         fFontInfo.family, altFontReader);
     if (glyphIdx > 0) then